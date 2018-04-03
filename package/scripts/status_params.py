e724fbf2f953816ab400da5e39b0500e769b852a
leilin <1216127658@qq.com>
2018-04-03 15:59:35 +0800

    change kibana pid file.

--- package/scripts/status_params.py
+++ package/scripts/status_params.py
@@ -6,5 +6,6 @@ from resource_management.libraries.script.script import Script
 
 config = Script.get_config()
 
-kibana_pid_dir = config['configurations']['kibana-site']['pid.file']
-kibana_pid_file = format("{kibana_pid_dir}/kibana.pid")
+# kibana_pid_file = config['configurations']['kibana-site']['pid.file']
+# kibana_pid_file = format("{kibana_pid_dir}/kibana.pid")
+kibana_pid_file = '/var/run/kibana.pid'
